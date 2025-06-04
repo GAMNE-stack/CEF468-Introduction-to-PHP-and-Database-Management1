eb6b8db1010cc694e98d5881ad393397e2c2322e
GAMNE-stack <gamnemanelle853@gmail.com>
2025-06-04 01:24:38 +0100

    Delete Webapp directory

--- Webapp/user_form.php
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="process_form.php">
-    Name: <input type="text" name="name" required><br>
-    Email: <input type="email" name="email" required><br>
-    Age: <input type="number" name="age" required><br>
-    <input type="submit" value="Submit">
-</form>
-
